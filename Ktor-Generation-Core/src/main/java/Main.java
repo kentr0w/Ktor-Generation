062ddc2793d80c923c8ec20edc0ed3575b93f914
kentr0w <qbextted0@gmail.com>
2021-04-21 19:33:47 +0300

    Socket Feature
    ### What's done:
    Implemented feature
    Added templates

--- Ktor-Generation-Core/src/main/java/Main.java
+++ Ktor-Generation-Core/src/main/java/Main.java
@@ -6,20 +6,7 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        /*System.out.println(DigestUtils.sha256Hex("db-feature-preHost"));
-        System.out.println(DigestUtils.sha256Hex("db-feature-hostName"));
-        System.out.println(DigestUtils.sha256Hex("db-feature-port"));
-        System.out.println(DigestUtils.sha256Hex("db-feature-dbName"));
-        System.out.println(DigestUtils.sha256Hex("db-feature-dbDriver"));
-        System.out.println(DigestUtils.sha256Hex("db-feature-username"));
-        System.out.println(DigestUtils.sha256Hex("db-feature-password"));*/
-    
         Stream.of("dbRoutingApplicationName", "dbRoutingEntityNamePath", "dbStandardRoutingCode").map(DigestUtils::sha256Hex).forEach(it -> {
-            System.out.println(it);
-        });
-        System.out.println();
-    
-        Stream.of("routingVariableName", "routingEntityName", "routingEntityName", "routingCopyVariableCode").map(DigestUtils::sha256Hex).forEach(it -> {
             System.out.println(it);
         });
         
