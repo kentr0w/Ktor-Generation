eaee077aec0c9d7dcd2d13f26e4db30a7e8e7244
Denis Kumar <qbextted0@gmail.com>
2021-05-06 16:40:56 +0300

    Config Search (#43)
    ### What's done:
    Created logic to scan config files

--- Ktor-Generation-Core/src/main/java/Main.java
+++ Ktor-Generation-Core/src/main/java/Main.java
@@ -1,10 +1,6 @@
 import Handler.Core;
 import org.apache.commons.codec.digest.DigestUtils;
 
-import java.io.File;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Main {
     public static void main(String[] args) {
         Core core = new Core();
